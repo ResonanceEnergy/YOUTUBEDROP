de08e845f7bb533f590b0a3a999fdfac8ba3a12a
OPTIMUS <optimus@resonanceenergy.com>
2026-02-27 06:16:14 -0700

    build: add setup.py [OPTIMUS]

---
+++ setup.py
@@ -0,0 +1,17 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="youtubedrop",
+    version="0.1.0",
+    description="Part of the ResonanceEnergy enterprise portfolio",
+    author="Resonance Energy",
+    author_email="optimus@resonanceenergy.com",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
